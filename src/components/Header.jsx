673a217f2b96047dd9c2568bb545884189816be6
Govind Sen <govindsen0915@gmail.com>
2024-06-12 11:54:14 +0530

    feat: add more categories on tap of 3 dot

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   HiEllipsisVertical,
   HiMiniBars3BottomLeft,
   HiMiniBars3BottomRight,
 } from "react-icons/hi2";
 import { Link } from "react-router-dom";
+import { GifState } from "../context/GifContext";
 
 const Header = () => {
   const [categories, setCategories] = useState([]);
   const [showCategories, setShowCategories] = useState(false);
+
+  const { gf, filter, setFilter, favourites } = GifState();
+
+  const fetchGifcategories = async () => {
+    const { data } = await gf.categories();
+    setCategories(data);
+  };
+
+  useEffect(() => {
+    fetchGifcategories();
+  }, []);
 
   return (
     <nav>
@@ -20,36 +32,58 @@ const Header = () => {
           </h1>
         </Link>
         <div className="font-bold text-md flex gap-2 items-center">
+          {/* render categories */}
+          {categories.slice(0, 5).map((category) => {
+            return (
+              <Link
+                key={category.name}
+                to={`/${category.name_encoded}`}
+                className="px-4 py-1 hover:gradient border-b-4 hidden lg:block"
+              >
+                {category.name}
+              </Link>
+            );
+          })}
 
-        <Link className="px-4 py-1 hover:gradient border-b-4 hidden lg:block">
-          Reactions
-        </Link>
-        <button onClick={() => setShowCategories(!showCategories)}>
-          <HiEllipsisVertical
-            size={35}
-            className={`py-0.5 hover:gradient ${
-              showCategories ? "gradient" : ""
-            } border-b-4 hidden lg:block`}
-          />
-        </button>
-        <div className="h-9 bg-gray-700 pt-1.5 px-6 cursor-pointer rounded">
-          <Link to="/favourites">Favourite GIFs</Link>
+          <button onClick={() => setShowCategories(!showCategories)}>
+            <HiEllipsisVertical
+              size={35}
+              className={`py-0.5 hover:gradient ${
+                showCategories ? "gradient" : ""
+              } border-b-4 hidden lg:block`}
+            />
+          </button>
+
+          {favourites.length > 0 && (
+            <div className="h-9 bg-gray-700 pt-1.5 px-6 cursor-pointer rounded">
+              <Link to="/favourites">Favourite GIFs</Link>
+            </div>
+          )}
+          <button>
+            <HiMiniBars3BottomRight className="text-sky-400 block lg:hidden size={30}" />
+          </button>
+          {/* </div>// */}
         </div>
-        <button>
-          <HiMiniBars3BottomRight className="text-sky-400 block lg:hidden size={30}" />
-        </button>
-      {/* </div>// */}
-      </div>
 
-      {showCategories && (
-        <div className="absolute right-0 top-14 px-10 pt-6 pb-9 w-full gradient z-20">
-          <span>Categories</span>
-          <hr />
-          <div>
-            <Link className="font-bold">Reactions</Link>
+        {showCategories && (
+          <div className="absolute right-0 top-14 px-10 pt-6 pb-9 w-full gradient z-20">
+            <span>Categories</span>
+            <hr />
+            <div>
+              {categories.map((category) => {
+                return (
+                  <Link
+                    className="font-bold"
+                    key={category.name}
+                    to={`/${category.name_encoded}`}
+                  >
+                    {category.name}
+                  </Link>
+                );
+              })}
+            </div>
           </div>
-        </div>
-      )}
+        )}
       </div>
     </nav>
   );
