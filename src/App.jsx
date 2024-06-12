508c36f7ad5d8f88feb9d8439b18dfa8cb8bb5d4
Govind Sen <govindsen0915@gmail.com>
2024-06-12 09:19:30 +0530

    feat: added multiple routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,46 @@
 
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import AppLayout from './layouts/AppLayout'
+import Home from './pages/Home'
 import './App.css'
+import Category from './pages/Category'
+import Search from './pages/Search'
+import GifPage from './pages/GifPage'
+import Favourites from './pages/Favourites'
+
+const router = createBrowserRouter([
+    {
+      element: <AppLayout />,
+
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/:category",
+          element: <Category />
+        },
+        {
+          path: "/search/:query",
+          element: <Search />
+        },
+        {
+          path: "/:type/:slug",
+          element: <GifPage />
+        },
+        {
+          path: "/favourites",
+          element: <Favourites />
+        },
+      ]
+    }
+])
 
 function App() {
 
   return (
-    <>
-      <h2 className='text-center'>GifBlitz</h2>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
